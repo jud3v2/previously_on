3ee4112c452c3862eb2200153f365aac571e1d7b
jud3v2 <judikaelbellance@icloud.com>
2024-11-06 14:05:37 +0100

    [+] mise à jour de la configuration pour la mise en production

--- frontend/src/config/index.ts
+++ frontend/src/config/index.ts
@@ -1,8 +1,8 @@
 export const frontendConfig = {
     betaSeriesAuthUrl: 'https://www.betaseries.com/authorize',
-    callbackUrl: 'http://localhost:3000/callback',
+    callbackUrl: 'https://previously-on.jud3v.fr/callback',
     apiBaseUrl: 'http://localhost:5500/api',
-    frontendBaseUrl: 'http://localhost:3000',
+    frontendBaseUrl: 'https://previously-on.jud3v.fr/',
     betaSeriesApiKey: 'a473506e85cc',
     betaSeriesOauth2Key: '86e612c3a3caf87827bfdf4645bfaddb',
     betaSeriesApiUrl: 'https://api.betaseries.com',
